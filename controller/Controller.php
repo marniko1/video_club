9e53a778c6e70b0679a57c31a8a51ad31e13c735
marniko1 <zmajeviognjenogvuka@gmail.com>
2018-04-18 14:21:07 +0200

    css

--- controller/Controller.php
+++ controller/Controller.php
@@ -11,14 +11,7 @@ class Controller {
 		require 'view/includes/footer.php';
 	}
 	public static function preparePaginationLinks($total_num, $pg) {
-		// if ($pg == 0) {
-		// 	$division = 1;
-		// } else {
-		// 	$division = $pg;
-		// }
 		$pg_num = ceil($total_num/2);
-		// var_dump($pg);
-		// var_dump($pg_num);
 		$links = array();
 		if ($pg_num >= 3) {
 			if ($pg <= 1) {
@@ -52,33 +45,6 @@ class Controller {
 				array_push($links, ['p1', "Next"]);
 			}
 		}
-		// if ($pg != 0) {
-		// 	for ($i=1; $i <= $pg_num; $i++) { 
-		// 		array_push($links, ['p'.$pg, $pg]);
-		// 	}
-		// } else {
-		// 	for ($i=1; $i <= $pg_num; $i++) { 
-		// 		array_push($links, ['p'.$i, $i]);
-		// 	}
-		// }
-		// if ($pg_num < 3) {
-		// 	for ($i=1; $i <= $pg_num; $i++) { 
-		// 		array_push($links, ['p'.$i, $i]);
-		// 	}
-		// } else {
-		// 	for ($i=1; $i <= 3; $i++) { 
-		// 		array_push($links, ['p'.$i, $i]);
-		// 	}
-		// }
-		// if ($pg < $pg_num) {
-		// 	if($pg == 'index') {
-		// 		array_push($links, ['p2', 'Next']);
-		// 	} else {
-		// 		array_push($links, ['p'.$pg, 'Next']);
-		// 	}
-		// } else {
-		// 	array_push($links, ['p'.$pg, "Next"]);
-		// }
 		return $links;
 	}
 }
--- view/includes/header.php
+++ view/includes/header.php
@@ -3,6 +3,7 @@
 	<head>
 		<title><?php echo $this->data['title']; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php  echo INCL_PATH.'assets/css/bootstrap.css'?>">
+		<link rel="stylesheet" type="text/css" href="<?php  echo INCL_PATH.'assets/css/main.css'?>">
 		<script type="text/javascript" src="<?php  echo INCL_PATH.'assets/js/jquery-3.3.1.js'?>"></script>
 		<script type="text/javascript" src="<?php  echo INCL_PATH.'assets/js/bootstrap.js'?>"></script>
 		<script type="text/javascript" src="<?php  echo INCL_PATH.'assets/ajax/ajax_req.js'?>"></script>
